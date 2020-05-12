ce6007471dcb87fe27cbc3a3154cdde3b6fbcac6
Esteban Santiago <esteban_santiago@MacBook-Pro-de-Esteban.local>
2020-05-12 17:35:26 +0200

    Ok tests but ....

--- dao/stock_client.js
+++ dao/stock_client.js
@@ -6,44 +6,48 @@ const StringHelper = require('../helpers/string');
 let stringHelper = new StringHelper();
 
 class StockClient {
-    constructor(stock, token) {
-        this.headers = {
-            Accept: 'application/json',
-            Authorization: 'Bearer ' + token
-        };
 
-        this.getBasicData = function () {
-            api_client.get(stringHelper.replaceInUrl(process.env.IOL_API_GET_STOCK, stock.mercado, stock.simbolo), { 'headers': this.headers }).then(resp => {
-                console.log(resp);
-            }).catch(error => {
-                console.log(error);
-            });
-        };
+    headers = {
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + token
+    };
+    constructor(stock, token) { }
 
-        this.getPriceData = function () {
-            return api_client.get(stringHelper.replaceInUrl(process.env.IOL_API_PRICE_STOCK, stock.mercado, stock.simbolo), { 'headers': this.headers }).catch(error => {
-                console.log(error);
-            });
-        };
-
-        this.buy = function (quantity, price, validity) {
-            const body = {
-                mercado: stock.mercado,
-                simbolo: stock.simbolo,
-                cantidad: quantity,
-                precio: price,
-                plazo: stock.plazo,
-                validez: validity //'2020-05-11'
-            };
-            return api_client.post(process.env.IOL_API_BUY_STOCK, body, { 'headers': this.headers }).catch(error => console.log(error));
-        };
-
-        this.getOptionsList = function () {
-            return api_client.get(stringHelper.replaceInUrl(process.env.IOL_API_GET_OPTIONS, stock.mercado, stock.simbolo), 
-                { 'headers': this.headers }
-            ).catch(error => {
-                console.log(error);
-            });
-        };
+    getStockProperties(ticker) {
+        return stocks.find(element => element.simbolo == ticker);
     }
+
+    getBasicData() {
+        api_client.get(stringHelper.replaceInUrl(process.env.IOL_API_GET_STOCK, stock.mercado, stock.simbolo), { 'headers': this.headers }).then(resp => {
+            console.log(resp);
+        }).catch(error => {
+            console.log(error);
+        });
+    };
+
+    getPriceData() {
+        return api_client.get(stringHelper.replaceInUrl(process.env.IOL_API_PRICE_STOCK, stock.mercado, stock.simbolo), { 'headers': this.headers }).catch(error => {
+            console.log(error);
+        });
+    };
+
+    buy(quantity, price, validity) {
+        this.body = {
+            mercado: stock.mercado,
+            simbolo: stock.simbolo,
+            cantidad: quantity,
+            precio: price,
+            plazo: stock.plazo,
+            validez: validity //'2020-05-11'
+        };
+        return api_client.post(process.env.IOL_API_BUY_STOCK, body, { 'headers': this.headers }).catch(error => console.log(error));
+    };
+
+    getOptionsList() {
+        return api_client.get(stringHelper.replaceInUrl(process.env.IOL_API_GET_OPTIONS, stock.mercado, stock.simbolo),
+            { 'headers': this.headers }
+        ).catch(error => {
+            console.log(error);
+        });
+    };
 }
